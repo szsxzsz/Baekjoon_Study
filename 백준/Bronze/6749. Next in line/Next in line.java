42d755e0d85b0b09b9718dc08ff4a036e00ba368
szsxzsz <111044993+szsxzsz@users.noreply.github.com>
2022-10-17 10:14:21 +0900

    [Bronze IV] Title: Next in line, Time: 212 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		int C = B-A;
+		System.out.println(B+C);
+		
+		scan.close();
+
+	}
+
+}
