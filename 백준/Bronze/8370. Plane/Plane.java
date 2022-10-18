a4a0c91a4c10bc538df8f2236b0ef31d3f6737ca
szsxzsz <111044993+szsxzsz@users.noreply.github.com>
2022-10-19 08:58:24 +0900

    [Bronze V] Title: Plane, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = scan.nextInt();
+		
+		System.out.println(a*b+c*d);
+		
+		scan.close();
+
+	}
+
+}
