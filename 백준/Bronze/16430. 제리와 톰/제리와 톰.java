446e73e0fa87f96425190f7e84a13abf00969f39
szsxzsz <111044993+szsxzsz@users.noreply.github.com>
2022-11-03 09:11:50 +0900

    [Bronze V] Title: 제리와 톰, Time: 224 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.print((b-a)+" ");
+		System.out.print(b);
+		
+		scan.close();
+
+	}
+
+}
