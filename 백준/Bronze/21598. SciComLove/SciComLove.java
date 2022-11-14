4f001a9a385f2264c70e40df47474bb4fb0431d6
szsxzsz <111044993+szsxzsz@users.noreply.github.com>
2022-11-14 10:01:29 +0900

    [Bronze V] Title: SciComLove, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		int n = scan.nextInt();
+		
+		for(int i = 0; i<n; i++) {
+			System.out.println("SciComLove");
+		}
+		
+		scan.close();
+
+	}
+
+}
