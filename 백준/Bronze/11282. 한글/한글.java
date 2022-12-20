811b6fed70e535fb49fdc2cdd1482f6165660569
szsxzsz <111044993+szsxzsz@users.noreply.github.com>
2022-12-20 10:11:14 +0900

    [Bronze IV] Title: 한글, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		a += 44031;
+		char b = (char)a;
+		System.out.println(b);
+
+}
+}
